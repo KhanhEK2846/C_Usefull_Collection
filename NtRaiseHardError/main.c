ac98df69838ef80979681f02eb27bdd5cc3ebb26
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2025-08-26 07:19:07 +0700

    Add NtRaiseHardError

---
+++ NtRaiseHardError/main.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <ntstatus.h>
+#pragma comment(lib, “ntdll.lib”)
+#define SHUTDOWN_PRIVILEGE 19
+#define OPTION_SHUTDOWN 6
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS, ULONG, PULONG_PTR, ULONG, ULONG, PULONG);
+
+int main()
+{
+    BOOLEAN Enabled;
+    RtlAdjustPrivilege(SHUTDOWN_PRIVILEGE, TRUE, FALSE, &Enabled);
+    if (Enabled == 0) {
+        ULONG response;
+        NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, OPTION_SHUTDOWN, &response);
+    }
+}
