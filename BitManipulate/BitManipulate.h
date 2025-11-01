28f528770921f04f4b69052756bc28264ddf1634
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2025-11-01 07:59:59 +0700

    Update bit manipulation

--- BitManipulate/BitManipulate.h
+++ BitManipulate/BitManipulate.h
@@ -125,3 +125,179 @@ inline boolean AreAnyBitsClearU32(const uint32_t value, const uint32_t mask) {
 inline boolean AreAnyBitsClearU64(const uint64_t value, const uint64_t mask) {
     return ((value & mask) != mask);
 }
+
+/*----------------------------------Check ith bit set-------------------------------*/
+inline boolean IsIthBitSetU8(const uint8_t value, const uint8_t position){
+    if(position > 7) return false;
+    return (value & GetSingleBitMaskU8(position));
+}
+
+inline boolean IsIthBitSetU16(const uint16_t value, const uint16_t position){
+    if(position > 15) return false;
+    return (value & GetSingleBitMaskU16(position));
+}
+
+inline boolean IsIthBitSetU32(const uint32_t value, const uint32_t position){
+    if(position > 31) return false;
+    return (value & GetSingleBitMaskU32(position));
+}
+
+inline boolean IsIthBitSetU64(const uint64_t value, const uint64_t position){
+    if(position > 63) return false;
+    return (value & GetSingleBitMaskU64(position));
+}
+
+/*----------------------------------Set ith bit-------------------------------*/
+inline uint8_t SetIthBitU8(const uint8_t value, const uint8_t position){
+    return SetBitsU8(value, GetSingleBitMaskU8(position));
+}
+
+inline uint16_t SetIthBitU16(const uint16_t value, const uint16_t position){
+    return SetBitsU16(value, GetSingleBitMaskU16(position));
+}
+
+inline uint32_t SetIthBitU32(const uint32_t value, const uint32_t position){
+    return SetBitsU32(value, GetSingleBitMaskU32(position));
+}
+
+inline uint64_t SetIthBitU64(const uint64_t value, const uint64_t position){
+    return SetBitsU64(value, GetSingleBitMaskU64(position));
+}
+
+/*----------------------------------Clear ith bit-------------------------------*/
+inline uint8_t ClearIthBitU8(const uint8_t value, const uint8_t position){
+    return ClearBitsU8(value, GetSingleBitMaskU8(position));
+}
+
+inline uint16_t ClearIthBitU16(const uint16_t value, const uint16_t position){
+    return ClearBitsU16(value, GetSingleBitMaskU16(position));
+}
+
+inline uint32_t ClearIthBitU32(const uint32_t value, const uint32_t position){
+    return ClearBitsU32(value, GetSingleBitMaskU32(position));
+}
+
+inline uint64_t ClearIthBitU64(const uint64_t value, const uint64_t position){
+    return ClearBitsU64(value, GetSingleBitMaskU64(position));
+}
+/*----------------------------------Toggle ith bit-------------------------------*/
+inline uint8_t ToggleIthBitU8(const uint8_t value, const uint8_t position){
+    return ToggleBitsU8(value, GetSingleBitMaskU8(position));
+}
+
+inline uint16_t ToggleIthBitU16(const uint16_t value, const uint16_t position){
+    return ToggleBitsU16(value, GetSingleBitMaskU16(position));
+}
+
+inline uint32_t ToggleIthBitU32(const uint32_t value, const uint32_t position){
+    return ToggleBitsU32(value, GetSingleBitMaskU32(position));
+}
+
+inline uint64_t ToggleIthBitU64(const uint64_t value, const uint64_t position){
+    return ToggleBitsU64(value, GetSingleBitMaskU64(position));
+}
+
+/*----------------------------------Remove Right Most Bit-------------------------------*/
+inline uint8_t RemoveRightMostBitU8(const uint8_t value){
+    return (value & (value - 1));
+}
+
+inline uint16_t RemoveRightMostBitU16(const uint16_t value){
+    return (value & (value - 1));
+}
+
+inline uint32_t RemoveRightMostBitU32(const uint32_t value){
+    return (value & (value - 1));
+}
+
+inline uint64_t RemoveRightMostBitU64(const uint64_t value){
+    return (value & (value - 1));
+}
+
+/*----------------------------------Is Even-------------------------------*/
+inline boolean IsEvenU8(const uint8_t value){
+    return (value & 1) == 0;
+}
+
+inline boolean IsEvenU16(const uint16_t value){
+    return (value & 1) == 0;
+}
+
+inline boolean IsEvenU32(const uint32_t value){
+    return (value & 1) == 0;
+}
+
+inline boolean IsEvenU64(const uint64_t value){
+    return (value & 1) == 0;
+}
+
+/*----------------------------------Is Odd-------------------------------*/
+inline boolean IsOddU8(const uint8_t value){
+    return (value & 1) != 0;
+}
+
+inline boolean IsOddU16(const uint16_t value){
+    return (value & 1) != 0;
+}
+
+inline boolean IsOddU32(const uint32_t value){
+    return (value & 1) != 0;
+}
+
+inline boolean IsOddU64(const uint64_t value){
+    return (value & 1) != 0;
+}
+
+/*----------------------------------Is Power 2-------------------------------*/
+inline boolean IsPower2U8(const uint8_t value){
+    return !(value & (value - 1));
+}
+
+inline boolean IsPower2U16(const uint16_t value){
+    return !(value & (value - 1));
+}
+
+inline boolean IsPower2U32(const uint32_t value){
+    return !(value & (value - 1));
+}
+
+inline boolean IsPower2U64(const uint64_t value){
+    return !(value & (value - 1));
+}
+
+/*----------------------------------Total of set bit-------------------------------*/
+inline uint8_t TotalSetBitsU8(uint8_t value){
+    uint8_t count = 0;
+    while(value){
+        value &= (value - 1);
+        count++;
+    }
+    return count;
+}
+
+inline uint8_t TotalSetBitsU16(uint16_t value){
+    uint8_t count = 0;
+    while(value){
+        value &= (value - 1);
+        count++;
+    }
+    return count;
+}
+
+inline uint8_t TotalSetBitsU32(uint32_t value){
+    uint8_t count = 0;
+    while(value){
+        value &= (value - 1);
+        count++;
+    }
+    return count;
+}
+
+inline uint8_t TotalSetBitsU64(uint64_t value){
+    uint8_t count = 0;
+    while(value){
+        value &= (value - 1);
+        count++;
+    }
+    return count;
+}
